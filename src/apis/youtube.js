5776910ae6b39bbf92e3d70a2eef0fc7953a8aac
tony trinh <tony.w.trinh3@gmail.com>
2019-04-29 22:04:11 -0700

    base website done

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -9,6 +9,7 @@ export default axios.create({
         part: 'snippet',
         maxResults: 5,
         key: KEY
-    }
+    },
+
 
 });
--- src/component/App.js
+++ src/component/App.js
@@ -2,10 +2,18 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
-
+import VideoDetail from './VideoDetail';
 
 class App extends Component {
   state = { videos: [], selectedVideo: null};
+
+
+  //to have the website automatically load something...
+  componentDidMount(){
+    this.onTermSubmit('bts')
+
+
+  }
 
 
   onTermSubmit = async (term) =>{
@@ -16,12 +24,17 @@ class App extends Component {
       }
 
     });
-    this.setState({videos: response.data.items});
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   //call back function...
   onVideoSelect = (video) =>{
     console.log('From the App!', video);
+
+    this.setState({selectedVideo: video});
   }
 
 
@@ -29,7 +42,22 @@ class App extends Component {
     return (
       <div className = 'ui container'>
         <SearchBar onFormSubmit = {this.onTermSubmit}/>
-        <VideoList onVideoSelect = {this.onVideoSelect} videos ={this.state.videos}/>
+        <div className = 'ui grid'>
+          <div className = 'ui row'>
+            <div className = 'eleven wide column'>
+              <VideoDetail video = {this.state.selectedVideo}/>
+            </div>
+
+            <div className = "five wide column">
+              <VideoList onVideoSelect = {this.onVideoSelect} videos ={this.state.videos}/>
+            </div>
+
+
+
+          </div>
+        </div>
+
+
 
       </div>
     );
